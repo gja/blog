20c04dd0a3e7e31702d16e3e7328ad5615f2eaf6
Tejas Dinkar <tejas@gja.in>
2012-07-04 00:00:13 +0530

    Get all hacker news like

--- bloggit.rb
+++ bloggit.rb
@@ -1,26 +1,67 @@
 #! /usr/bin/env ruby
 
+require 'optparse'
 require 'redcarpet'
 require 'restclient'
-
-file = ARGV[0] ? File.open(ARGV[0]) : STDIN
+require 'cgi'
 
 class Bloggit < Redcarpet::Render::HTML
+  def initialize(options)
+    super
+    @hacker_news_id = options[:hacker_news]
+  end
+
+  def fetch(url)
+    $stderr.puts("Fetching #{url}")
+    CGI.escapeHTML RestClient.get(url)
+  end
+
   def replace_gists(document)
     document.gsub(/\[gist (.*) (.*)\]/) do
       gist_id = $1
       file = $2
       <<-EOF
 <script src="https://gist.github.com/#{gist_id}.js?file=#{file}"></script>
-<noscript>#{RestClient.get("https://raw.github.com/gist/#{gist_id}/#{file}")}</noscript>
+<noscript>#{fetch("https://raw.github.com/gist/#{gist_id}/#{file}")}</noscript>
 EOF
     end
   end
 
+  def hacker_news_snippet
+    @hacker_news_id.nil? ? "" :  <<-EOF
+<div style="text-align: center;">
+  <b><a href="http://news.ycombinator.com/item?id=#{@hacker_news_id}">upvote it</a> on Hacker News</b>
+</div>
+EOF
+  end
+
+  def doc_footer
+    <<-EOF
+<hr />
+<b>If you liked this post, you could:</b><br />
+<h4 style="text-align: center;">
+  <a class="twitter-follow-button" data-show-count="false" href="https://twitter.com/tdinkar">Follow @tdinkar</a>
+</h4>
+#{hacker_news_snippet}
+<br/>
+EOF
+  end
+
   def postprocess(full_document)
-    [:replace_gists].inject(full_document) { |d, m| send(m, d) }
+    methods = []# [:replace_gists]
+    methods.inject(full_document) { |d, m| send(m, d) }
   end
 end
 
-markdown = Redcarpet::Markdown.new(Bloggit.new)
+render_opts = {:with_toc_data => true}
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: blogit input.md [--hacker-news 4194066]"
+  opts.on('-hn', '--hacker-news ID', "Link to hacker news") do |id| 
+    render_opts[:hacker_news] = id
+  end
+end
+optparse.parse!
+
+file = ARGV[0] ? File.open(ARGV[0]) : $stdin
+markdown = Redcarpet::Markdown.new(Bloggit.new(render_opts), :autolink => true)
 puts markdown.render file.read
